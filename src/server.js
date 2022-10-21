d9fc526116bea701cebeb3c5154272a3b78f4833
Kim-seung-hyun98 <100749004+Kim-seung-hyun98@users.noreply.github.com>
2022-10-21 21:45:06 +0900

    zoom

--- src/server.js
+++ src/server.js
@@ -15,9 +15,20 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
+  socket.onAny((event) => {
+    console.log(`Socket Event : ${event}`);
+  });
   socket.on("enter_room", (roomName, done) => {
-    console.log(roomName);
-    done("hello form backend");
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("Hello");
+  });
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+  });
+  socket.on("new_message", (msg, room, done) => {
+    socket.to(room).emit("new_message", msg);
+    done();
   });
 });
 
